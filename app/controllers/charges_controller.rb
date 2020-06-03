95414371eaec44af7933091d6da5f506fa915443
Tomaz <tmedrado99@gmail.com>
2020-06-03 10:20:40 -0300

    Add stripe button to cart index

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
   def create
       # Amount in cents
-    @amount = 500
+    @amount = 250000
 
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
