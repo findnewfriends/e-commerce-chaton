c0e2fb69b6462653b8318841a71cc38ce0ec6ddd
alexis lafrance <alexislafrances-laf@outlook.fr>
2020-06-04 00:02:28 +0200

    profile page beginning and delete some useless files

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   resources :items
   devise_for :users
   resources :users
+  resources :users, only:  :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
