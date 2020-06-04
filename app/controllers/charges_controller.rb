158048c376c1cf9d8af6994d5a3d0f663abd15a0
alexis lafrance <alexislafrances-laf@outlook.fr>
2020-06-04 18:46:45 +0200

    good amount in controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,26 +3,26 @@ class ChargesController < ApplicationController
   end
 
   def create
-      # Amount in cents
-      puts '-----------'
-      puts price
-      puts '---------'
-    @amount = 250000
+    @carts = Cart.all.where(user:current_user)
+    @amount = 0
+    @carts.each do |cart|
+      @amount += cart.item.price * cart.quantity
+    end
+    @amount = (@amount * 100).to_i
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+  
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+    Cart.all.where(user:current_user).destroy_all
 
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
-
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'usd',
-    })
-
-    rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+    flash[:success] = 'Paiement effecté'
+    redirect_to user_path(current_user.id)
   end
 end
